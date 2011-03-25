5be4fb89ca85edbddf44bd798242cd971c6fdbe6
Giovanni Bassi <giggio@giggio.net>
2011-03-25 15:10:44 -0300

    adicionando arquivos que faltaram

---
+++ src/BDD/Scenarios/features/step_definitions/ofertas_steps.rb.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+require_relative '../support/sql_server'
+
+Dado /^que há as seguintes ofertas:$/ do |dados|
+  # table is a |1|Sorveteria do Jorge|Sorvete de limão|produtos/sorvete.jpg|5|12|
+  server = SqlServer.obter_aberto
+  statements = []
+  dados.hashes.each do |hash|
+		o = Oferta.new hash
+    statements << "INSERT INTO [Oferta] ([Id], [NomeEmpresa], [ProdutoEmOferta], [UrlImagem], [ValorComDesconto], [ValorOriginal]) VALUES (#{o.id}, '#{o.nome_empresa}', '#{o.produto_em_oferta}', '#{o.url_imagem}', #{o.valor_com_desconto}, #{o.valor_original})"
+  end
+  server.execute_all statements
+  server.close
+end
---
+++ src/BDD/Scenarios/features/support/model/modelo_simples.rb
@@ -0,0 +1,3 @@
+class Oferta
+  hash_initializer :id, :nome_empresa, :produto_em_oferta, :url_imagem, :valor_com_desconto, :valor_original
+end
