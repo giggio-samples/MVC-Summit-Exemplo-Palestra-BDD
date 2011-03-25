7f03020462c421c9f1f99d68465a63959f6cfbd7
Giovanni Bassi <giggio@giggio.net>
2011-03-25 02:30:33 -0300

    arrumando nome do db

--- src/BDD/Scenarios/features/step_definitions/autenticacao_steps.rb.rb
+++ src/BDD/Scenarios/features/step_definitions/autenticacao_steps.rb.rb
@@ -7,7 +7,7 @@ end
 Dado /^que sou um usuário logado$/ do
   Dado "que eu estou na página Account Logon"
   E "eu preencho os dados:", table(%{
-    |UserName|giovanni@lambda3.com.br|
+    |UserName|Giovanni|
     |Password|microsoft|
   })
   E %{eu clico em "Logar"}
